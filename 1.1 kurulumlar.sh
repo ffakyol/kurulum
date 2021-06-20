6e3aeb0553975e874e5ab93469254a5902dae489
ffakyol <59020078+ffakyol@users.noreply.github.com>
2021-06-20 14:13:06 +0000

    Update and rename 1.1 kurulumlar.sh.sh to 1.1 kurulumlar.sh

---
+++ 1.1 kurulumlar.sh
@@ -0,0 +1,20 @@
+sudo apt-get install ros-noetic-gmapping
+sudo apt-get install ros-noetic-slam-*
+sudo apt-get install ros-noetic-effort-controllers
+sudo apt-get install ros-noetic-position-controllers
+sudo apt-get install ros-noetic-diff-drive-controller
+sudo apt-get install ros-noetic-cv-*
+sudo apt-get install ros-noetic-joy*
+sudo apt-get install ros-noetic-openni-*  
+sudo apt-get install ros-noetic-ackermann-*
+sudo apt-get install ros-noetic-control*
+sudo apt-get install ros-noetic-joint-*
+sudo apt-get install ros-noetic-gazebo-*
+sudo apt-get install ros-noetic-uvc-camera
+sudo apt-get install ros-noetic-usb-cam
+sudo apt-get install ros-noetic-find-object-2d
+sudo apt-get install python-opencv
+sudo apt-get install python3-opencv
+sudo apt-get install ros-noetic-dwa-local-planner
+
+
