b876d4b5ff63b9e133b6a8ade0e1ce5ddbf287e5
ffakyol <59020078+ffakyol@users.noreply.github.com>
2021-06-20 17:10:20 +0300

    Add files via upload

---
+++ 1.1 kurulumlar.sh.sh
@@ -0,0 +1,20 @@
+sudo apt-get install ros-melodic-gmapping
+sudo apt-get install ros-melodic-slam-*
+sudo apt-get install ros-melodic-effort-controllers
+sudo apt-get install ros-melodic-position-controllers
+sudo apt-get install ros-melodic-diff-drive-controller
+sudo apt-get install ros-melodic-cv-*
+sudo apt-get install ros-melodic-joy*
+sudo apt-get install ros-melodic-openni-*  
+sudo apt-get install ros-melodic-ackermann-*
+sudo apt-get install ros-melodic-control*
+sudo apt-get install ros-melodic-joint-*
+sudo apt-get install ros-melodic-gazebo-*
+sudo apt-get install ros-melodic-uvc-camera
+sudo apt-get install ros-melodic-usb-cam
+sudo apt-get install ros-melodic-find-object-2d
+sudo apt-get install python-opencv
+sudo apt-get install python3-opencv
+sudo apt-get install ros-melodic-dwa-local-planner
+
+
